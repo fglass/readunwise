11418ab706b60fe5a084e736dc6c75c4cdcbeacc
Fred Glass <glasshcs@gmail.com>
2023-01-31 23:20:44 +0000

    Use rich text formatting

--- src/readunwise.py
+++ src/readunwise.py
@@ -1,12 +1,19 @@
 import click
+import platform
 from click import Context
 from clippings import parse_clippings_file
 from random_util import select_random_book, select_random_highlights
+from rich import print as rprint
+from rich.panel import Panel
+from rich.table import Table
 from typing import List, Tuple
+
+DEFAULT_KINDLE_PATH = r"/Volumes/Kindle/" if platform.system() == "Darwin" else r"D:\\"
+DEFAULT_CLIPPINGS_FILE = rf"{DEFAULT_KINDLE_PATH}/documents/My Clippings.txt"
 
 
 @click.group()
-@click.option("--clippings_file", default=r"D:\documents\My Clippings.txt", help="Clippings file from Kindle device.")
+@click.option("--clippings_file", default=DEFAULT_CLIPPINGS_FILE, help="Clippings file from Kindle device.")
 @click.pass_context
 def cli(ctx: Context, clippings_file: str):
     ctx.ensure_object(dict)
@@ -16,8 +23,16 @@ def cli(ctx: Context, clippings_file: str):
 @cli.command(help="List books found in the clippings file.")
 @click.pass_context
 def ls(ctx: Context):
+    table = Table()
+    table.add_column("Index", justify="center", style="magenta")
+    table.add_column("Book Title")
+
     highlights_by_book = _get_highlights_by_book(ctx)
-    [print(f"{i + 1}: {book}") for i, book in enumerate(highlights_by_book.keys())]
+
+    for i, book in enumerate(highlights_by_book.keys()):
+        table.add_row(str(i + 1), book)
+
+    rprint(table)
 
 
 @cli.command(help="Display highlights from a book.")
@@ -28,11 +43,11 @@ def cat(ctx: Context, book: str):
     book = _arg_to_book(book, highlights_by_book)
 
     if book not in highlights_by_book:
-        print(f"No highlights found for {book}")
+        rprint(f"[b red]No highlights found for {book}")
         return
 
     for highlight in highlights_by_book[book]:
-        print(f"- {highlight.content}")
+        rprint(f"[magenta]-[/] {highlight.content}")
 
 
 @cli.command(help="Print a random highlight.")
@@ -46,15 +61,14 @@ def random(ctx: Context, ignore: Tuple[str]):
     book_highlights = highlights_by_book[random_book]
     selected_highlight = select_random_highlights(book_highlights, n=1)[0]
 
-    print("-" * 50)
-    print(f"{selected_highlight.content}\n\n- {random_book}")
-    print("-" * 50)
+    panel = Panel.fit(f"[b magenta]{selected_highlight.content}[/]\n\n- {random_book}")
+    rprint(panel)
 
 
 @cli.command(help="Send random highlights to a Discord channel.")
 @click.argument("auth_token")
 @click.argument("channel_id", type=click.INT)
-@click.option("--count", "-c", default=3, help="Number of highlights to select (default: 3).")
+@click.option("-n", default=3, help="Number of highlights to select (default: 3).")
 @click.option("--ignore", "-i", multiple=True, help="Book title or index to ignore.")
 @click.pass_context
 def discord(ctx: Context, auth_token: str, channel_id: int, count: int, ignore: Tuple[str]):
